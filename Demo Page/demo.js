253a34d0b6198f854fe5c8945833865a590984fe
SkertSki <bdube.business@gmail.com>
2024-11-20 18:16:04 -0700

    Some minor changes

---
+++ Demo Page/demo.js
@@ -0,0 +1,37 @@
+const img1 = new Image();
+const img2 = new Image();
+const img3 = new Image();
+const img4 = new Image();
+const img5 = new Image();
+const img6 = new Image();
+const img7 = new Image();
+const img8 = new Image();
+const img9 = new Image();
+
+img1.src = 'MommyandSunnySaltFlats3.JPG';
+img2.src = 'JacSmileInSettingSun.JPG';
+img3.src = 'GoingtoBirth.JPG';
+img4.src = 'MommyandSunnySunset3.JPG';
+img5.src = 'BirthMommySleepy.JPG';
+img6.src = 'JustgaveBirth.JPG';
+img7.src = 'MommyandSunnySaltFlats4.JPG';
+img8.src = 'MommyandSunnySunset2.JPG';
+img9.src = 'BrokenGlassSaltFlats.JPG';
+
+// img1.onload = () => {
+//     const picture = document.createElement('picture');
+//     const pic = picture.getContext('2d');
+
+//     const newW = img1.width / 5;
+//     const newH = img1.height / 5;
+
+//     pic.drawImage (img1, 0, 0, newWidth, newHeight);
+
+//     const lowQimgURL1 = picture.toDataURL('image/jpeg', 1);
+
+//     const lowQimg1 = new Image()
+//     lowQimg1.src = lowQimgURL1
+
+//     document.body.appendChild(lowQimg1)
+// }
+
